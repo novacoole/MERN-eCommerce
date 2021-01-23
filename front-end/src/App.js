39eca924daa8da8a71057019e6d1bc12c0ba2bde
nova <alexander.coole@protonmail.com>
2021-01-23 12:16:24 +1100

    react setup, initial components, configure .gitignore

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
-      <h1>Hello from ProShop</h1> 
+      <Header />
+      <Container>
+        <main className="py-3">
+          <h1>Hello from ProShop</h1>
+        </main>
+      </Container>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
